5cf2a814ff3da8b7add3afe49ceb92cfb2cebf05
Ashref <ashref.abderrahmane.berbaoui@ensia.edu.dz>
2025-12-25 15:08:44 +0100

    update ionicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,19 +28,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/logo.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/logo.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/images/logo.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/images/logo.svg",
   },
 }
 
